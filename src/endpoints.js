8500111392a60100bb2d60d2927ed88123f25ace
Ben Robbins <bd.robbins@gmail.com>
2016-08-06 22:01:23 -0400

    Update Destiny::Inventoryto the non-deprecated endpoint. Add Destiny::ItemDetail endpoint

--- src/endpoints.js
+++ src/endpoints.js
@@ -9,8 +9,9 @@ let GET = [
         { name: 'Account', url: '${ membershipType }/Account/${ membershipId }/', required: ['membershipType', 'membershipId'] },
         { name: 'Character', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/', required: ['membershipType', 'membershipId', 'characterId'] },
         { name: 'Activities', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/Activities/', required: ['membershipType', 'membershipId', 'characterId'] },
-        { name: 'Inventory', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/Inventory/', required: ['membershipType', 'membershipId', 'characterId'] },
+        { name: 'Inventory', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/Inventory/Summary/', required: ['membershipType', 'membershipId', 'characterId'] },
         { name: 'Progression', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/Progression/', required: ['membershipType', 'membershipId', 'characterId'] }
+        { name: 'ItemDetail', url: '${ membershipType }/Account/${ membershipId }/Character/${ characterId }/Inventory/${ itemInstanceId }', required: ['membershipType', 'membershipId', 'characterId', 'itemInstanceId'] }
     ].map(UTILS.assignMap({ options: { method: UTILS.METHODS.GET, headers: UTILS.HEADERS } }));
 
 /**
